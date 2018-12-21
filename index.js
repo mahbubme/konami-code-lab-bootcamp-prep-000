8c6f36145cc9e277db2731c3f74a485d4f9c18d3
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 06:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index=0;
 function init() {
   window.addEventListener("keydown", function(event){
-    const key=parseInt(event.which);
+    const key= event.key;
     
-    console.log(event.which);
+    console.log(event.key);
     
     if( key===code[index] ){
       index++;
