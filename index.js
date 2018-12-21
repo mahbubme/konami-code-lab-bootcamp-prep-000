560a4bda4ea37564e85c39791b8d733eb3f5f4bd
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 06:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index=0;
 
-function init() {
+function init( event ) {
     const key=parseInt(event.which);
+    
     console.log(event.key);
+    
     if(key===code[index]){
       index++;
       if(index===code.length){
