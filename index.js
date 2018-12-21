90fbd5e57fbf9cac7f59ccacb24f91762397b88f
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 06:37:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index=0;
 
 function init() {
-  document.body.addEventListener("keydown", function(event){
     const key=parseInt(event.which);
     console.log(event.which);
     if(key===code[index]){
@@ -15,6 +14,6 @@ function init() {
     else{
       index=0;
     }
-  })
 }
-init();
+
+document.addEvent
