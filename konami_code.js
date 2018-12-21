527405aab063e7cae55772d118031af888eb4e79
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 07:16:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,3 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
-
 let index=0;
 
 function init() {
