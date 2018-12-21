9606d12711d3e61b6d95d3361331c308f151c018
Mahbubur Rahman <mail@mahbub.me>
2018-12-21 06:42:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index=0;
-
-function init( event ) {
+function init() {
+  window.addEventListener("keydown", function(event){
     const key=parseInt(event.which);
-    
-    console.log(event.key);
-    
+    console.log(event.which);
     if(key===code[index]){
       index++;
       if(index===code.length){
-        alert("Congratualtions")
+        window.alert("Cheat activated, you cheater")
       }
     }
     else{
       index=0;
     }
+  })
 }
-
-document.body.addEventListener('keydown', init);
+init();
